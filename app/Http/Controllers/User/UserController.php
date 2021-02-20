24c3fe55df599e4c05d9fbc5b6f4e1f5400f0a4b
shofujimoto0617 <shofujimoto017@gmail.com>
2021-02-20 19:00:57 +0900

    Add Genre to Home

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Item;
+use App\Models\Genre;
 use Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -14,6 +15,8 @@ class UserController extends Controller
 {
     public function Home() {
         $items = Item::inRandomOrder()->take(9)->get();
-        return view('user.home', compact('items'));
+        $genres = Genre::inRandomOrder()->take(9)->get();
+
+        return view('user.home', compact('items', 'genres'));
     }
 }
